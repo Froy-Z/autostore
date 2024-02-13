e20705951aafc7249972c935b165f4ed1cd18694
Владислав Дьяченко <dyacenko_v_qschool@qsoft.ru>
2024-02-13 09:06:32 +0000

    278890 / Интеграция главной страница: - создан футер и хедер (вынесены отдельно в components) - выделен контент - стили и скрипты подгружаются npm - удалены страницы и файлы не относящиеся к заданию

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,3 @@ use App\Http\Controllers\PagesController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PagesController::class, 'home']);
-
-Route::get('/catalog', [CatalogController::class, 'catalog']);
-
-Route::get('/product/{product}', [CatalogController::class, 'product']);
-
