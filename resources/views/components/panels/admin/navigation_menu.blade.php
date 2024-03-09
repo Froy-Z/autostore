5c257c902ec4e19a5d18ac0574f543a6ea19abe6
Владислав Дьяченко <dyacenko_v_qschool@qsoft.ru>
2024-03-09 19:03:58 +0000

    278904 / Меню категорий каталога - небольшие доработки;

--- resources/views/components/panels/admin/navigation_menu.blade.php
+++ resources/views/components/panels/admin/navigation_menu.blade.php
@@ -1,6 +1,7 @@
 <nav class="order-1">
     <ul class="block lg:flex">
-        <li class="group"><a class="inline-block p-4 text-black font-bold hover:text-orange" href="admin_cars.html">Модели</a></li>
+        <li class="group"><a class="inline-block p-4 text-black font-bold hover:text-orange" href="{{ route('admin.admin') }}">Админ</a></li>
+        <li class="group"><a class="inline-block p-4 text-black font-bold hover:text-orange" href="#">Модели</a></li>
         <li class="group"><a class="inline-block p-4 text-black font-bold hover:text-orange" href="{{ route('admin.articles.index') }}">Новости</a></li>
         <li class="group"><a class="inline-block p-4 text-black font-bold hover:text-orange" href="{{ route('home') }}">На сайт</a></li>
     </ul>
